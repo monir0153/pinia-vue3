484465b3d708fd159b9e7bae820d30143819f7c5
monir0153 <mohammadmonir0153@gmail.com>
2024-05-19 10:42:08 +0600

    state reloading problem solve & add reset button

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@
     <main>
       <!-- Heading -->
       <header class="text-white mt-5">
-        <img src="./assets/logo.svg" alt="pinia logo" srcset="" width="100px" class="mx-auto">
+        <img
+          src="./assets/logo.svg"
+          alt="pinia logo"
+          srcset=""
+          width="100px"
+          class="mx-auto"
+        />
         <p class="text-2xl font-semibold text-center">Pinia Task</p>
       </header>
       <!-- Add new task -->
@@ -12,22 +18,40 @@
       </div>
       <!-- nav -->
       <nav class="text-center text-white">
-        <button class="px-3 py-2 bg-slate-700 m-3 rounded-lg" @click="filter = 'all'">all task</button>
-        <button class="px-3 py-2 bg-slate-700 m-3 rounded-lg" @click="filter = 'favs'">Favorite</button>
+        <button
+          class="px-3 py-2 bg-slate-700 m-3 rounded-lg"
+          @click="filter = 'all'"
+        >
+          all task
+        </button>
+        <button
+          class="px-3 py-2 bg-slate-700 m-3 rounded-lg"
+          @click="filter = 'favs'"
+        >
+          Favorite
+        </button>
+        <button
+          class="px-3 py-2 bg-slate-700 m-3 rounded-lg"
+          @click="taskStore.$reset"
+        >
+          Reset state
+        </button>
       </nav>
       <!-- loading -->
       <!-- Task List -->
-      <div class="text-white w-5/12 mx-auto " v-if="filter === 'all'">
-        <p>you have {{ taskStore.totalCount }} task left to do</p>
-        <p class="text-center text-white text-5xl" v-if="taskStore.loading">loading task...</p>
-        <div v-for="task in taskStore.tasks" :key="task.id" class="py-5">
-          <TaskDetails :task="task"/>
+      <div class="text-white w-5/12 mx-auto" v-if="filter === 'all'">
+        <p>you have {{ totalCount }} task left to do</p>
+        <p class="text-center text-white text-5xl" v-if="loading">
+          loading task...
+        </p>
+        <div v-for="task in tasks" :key="task.id" class="py-5">
+          <TaskDetails :task="task" />
         </div>
       </div>
-      <div class="text-white w-5/12 mx-auto " v-if="filter === 'favs'">
-        <p>you have {{ taskStore.favsCount }} fav left to do</p>
-        <div v-for="task in taskStore.favs" :key="task.id" class="py-5">
-          <TaskDetails :task="task"/>
+      <div class="text-white w-5/12 mx-auto" v-if="filter === 'favs'">
+        <p>you have {{ favsCount }} fav left to do</p>
+        <div v-for="task in favs" :key="task.id" class="py-5">
+          <TaskDetails :task="task" />
         </div>
       </div>
     </main>
@@ -35,21 +59,24 @@
 </template>
 
 <script>
-import { ref } from 'vue';
-import TaskDetails from './components/TaskDetails.vue';
-import { useTaskStore } from './stores/TaskStore';
-import TaskForm from './components/TaskForm.vue';
+import { ref } from "vue";
+import TaskDetails from "./components/TaskDetails.vue";
+import { useTaskStore } from "./stores/TaskStore";
+import TaskForm from "./components/TaskForm.vue";
+import { storeToRefs } from "pinia";
 
-  export default {
-    components: {
-      TaskDetails,
-        TaskForm
-    },
-    setup() {
-      const taskStore = useTaskStore();
-      taskStore.getTasks();
-      const filter = ref('all')
-      return {taskStore, filter}
-    }
-  }
+export default {
+  components: {
+    TaskDetails,
+    TaskForm,
+  },
+  setup() {
+    const taskStore = useTaskStore();
+    const { tasks, loading, favs, totalCount, favsCount } =
+      storeToRefs(taskStore);
+    taskStore.getTasks();
+    const filter = ref("all");
+    return { taskStore, filter, tasks, loading, favs, totalCount, favsCount };
+  },
+};
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -26,17 +26,43 @@ export const useTaskStore = defineStore("taskStore", {
       this.tasks = data;
       this.loading = false;
     },
-    addTask(task) {
+    async addTask(task) {
       this.tasks.push(task);
+      const res = await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(task),
+      });
+      if (res.error) {
+        console.log(res.error);
+      }
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       this.tasks = this.tasks.filter((t) => {
         return t.id != id;
       });
+      const res = await fetch("http://localhost:3000/tasks/" + id, {
+        method: "DELETE",
+      });
+      if (res.error) {
+        console.log(res.error);
+      }
     },
-    toggleFav(id) {
+    async toggleFav(id) {
       const task = this.tasks.find((t) => t.id === id);
       task.isFav = !task.isFav;
+      const res = await fetch("http://localhost:3000/tasks/" + id, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ isFav: task.isFav }),
+      });
+      if (res.error) {
+        console.log(res.error);
+      }
     },
   },
 });
